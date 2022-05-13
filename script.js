61934c2b7b463c5c4b92b9160c5bf6cb93a42bc8
Bram Moreinis <bmoreini@bsdvt.org>
2022-05-13 09:55:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var attributes = [["Strength",0],["Intelligence",0],["Wisdom",0],["Constitution"
 // classReq = attributes[index], minimum value to qualify, classes[index]
 var classReq = [[0,11,0],[1,11,1],[2,11,2],[3,11,3],[4,11,4],[5,11,5]];
 var classes = [["Christian Bale",["Batman Begins, The Dark Night"],"One Punch Knockout"],[]];
+var classReq = [[0,13,0],[1,14,1],[2,9,2],[3,11,3],[4,10,4],[5,12,5]];
+var classes = [["Christian Bale",["Batman Begins", "The Dark Night"],"One Punch Knockout"],["Robert Pattinson",["The Batman 2020"],"Knows All The Answers"],["Michael Keaton",["Batman 1989"],"Predicts Villain Behaviors"],["Will Arnett",["Lego Batman: The Movie"],"No Fall Damage"],["Ben Affleck",["Batman vs. Superman"],"Can Escape Any Room"],["Kevin Conroy",["Batman: The Killing Joke"],"Soul Catching Voice"]];
 var choices = [];
 var whichAttribute = 0; // Which one are we on?
 var maxRolls = 3; // how many rerolls? Default = 3
